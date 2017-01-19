9e9e8cede093196f34c58b6ce50ab6609313293b
Pieta <pieta.tuomisto@helsinki.fi>
2017-01-19 15:18:27 +0200

    projektin muokkaus

---
+++ PromptGenerator/src/main/java/writingprompts/promptgenerator/Main.java
@@ -0,0 +1,10 @@
+package writingprompts.promptgenerator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Just keep writing");
+    }
+    
+}
